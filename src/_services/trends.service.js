dd5ae53f601661cfd6a5fba71366d4705b077f85
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2018-10-22 22:25:24 +0530

    ui changed endpoints changed

--- src/_services/trends.service.js
+++ src/_services/trends.service.js
@@ -18,7 +18,7 @@ function getTrends( trackers, parameter ) {
 
 
 
-    return fetch(`http://2e271ad6.ngrok.io/trends`, requestOptions)
+    return fetch(`http://${hostName}:5000/trends`, requestOptions)
         .then(handleResponse)
 }
 
